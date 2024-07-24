4115023eff960322210a94a2933784d02166d5ef
nkitan <ankitdas2k@gmail.com>
2024-07-25 02:15:12 +0530

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,12 @@
 
 echo "Setting up Enhanced-Lip-Reading"
 python3 -m venv env
-source ./env/bin/activate
 while getopts 'i' OPTION; do
 	case "$OPTION" in
 		i) echo "Installing Dependencies"
-			pip install -r requirements.txt 
+			./env/bin/pip install -r requirements.txt 
 			;;
 	esac
 done
 echo "successfully installed Enhanced-Lip-Reading at $(pwd)"
-python manage.py runserver &
+./env/bin/python manage.py runserver &
